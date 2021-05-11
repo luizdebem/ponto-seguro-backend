0b00bb77a8f932c8f8c06ef7eae9eaaae04727f3
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-05-11 00:32:20 -0300

    Login route

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,11 +5,14 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 // @TODO @luizdebem update, delete, retrieve, login e logout
+// @TODO @luizdebem validations
 
-router.post('/', async (req, res) => {
+router.post('/signup', async (req, res) => {
   const { fullName, email, password, isAdmin } = req.body;
-
   try {
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(400).json({ error: 'User already exists' });
+
     bcrypt.hash(password, 10, async (error, hash) => {
       if (error) throw error;
 
@@ -30,8 +33,33 @@ router.post('/', async (req, res) => {
           res.status(200).json({ data: { user, token } });
         }
       );
-      
+
     });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: 'Invalid credentials.' });
+
+    const matched = await bcrypt.compare(password, user.password);
+    if (!matched) return res.status(400).json({ error: 'Invalid credentials.' });
+
+    jwt.sign(
+      { id: user.id },
+      'jwt-secret',
+      { expiresIn: 3600 },
+      (error, token) => {
+        if (error) throw error;
+        res.status(200).json({ data: { user, token } });
+      }
+    );
+
   } catch (error) {
     res.status(400).json(error);
   }
