bb3f6339ddaa71bb7d76f0e29c1cfde06bd03d88
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2021-05-23 22:03:51 -0300

    updated report model

--- src/models/Report.js
+++ src/models/Report.js
@@ -18,48 +18,23 @@ const ReportSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  userType: {
+    type: String,
+    enum: ['PEDESTRIAN', 'DRIVER'],
+    required: true
+  },
   details: { 
     type: String,
     required: false
   },
-  type: {
+  reportType: {
     type: String,
     enum: ['THEFT', 'ROBBERY'],
     required: true
   },
-  date: {
+  when: {
     type: Date,
-    required: true
-  },
-  time: {
-    type: String,
-    enum: [
-      '00:00 - 01:00',
-      '01:00 - 02:00',
-      '02:00 - 03:00',
-      '03:00 - 04:00',
-      '04:00 - 05:00',
-      '05:00 - 06:00',
-      '06:00 - 07:00',
-      '07:00 - 08:00',
-      '08:00 - 09:00',
-      '09:00 - 10:00',
-      '10:00 - 11:00',
-      '11:00 - 12:00',
-      '12:00 - 13:00',
-      '13:00 - 14:00',
-      '14:00 - 15:00',
-      '15:00 - 16:00',
-      '16:00 - 17:00',
-      '17:00 - 18:00',
-      '18:00 - 19:00',
-      '19:00 - 20:00',
-      '20:00 - 21:00',
-      '21:00 - 22:00',
-      '22:00 - 23:00',
-      '23:00 - 00:00'
-    ],
-    required: true
+    required: false
   }
 }, { timestamps: true });
 
